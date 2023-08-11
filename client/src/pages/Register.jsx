3b5dac76a85e5885ec626ef4f019a4a4ac4a55bd
doanxuanduyen <doanxuanduyen1357@gmail.com>
2023-08-11 13:07:45 +0700

    change route home

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link,  useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 import { ToastContainer } from "react-toastify";
@@ -31,7 +31,7 @@ function Register() {
       }
       if (data.status === true) {
         localStorage.setItem("chat", JSON.stringify(data.user));
-        navigate("/chat");
+        navigate("/");
       }
     }
   };
